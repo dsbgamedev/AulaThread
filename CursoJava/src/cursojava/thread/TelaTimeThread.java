0f0f408a45e08c9f621e8f58b2126141df7a8442
Douglas <Douglas@Douglas>
2022-04-28 17:00:29 -0300

    Prineira parte tela pronta

--- CursoJava/src/cursojava/thread/TelaTimeThread.java
+++ CursoJava/src/cursojava/thread/TelaTimeThread.java
@@ -1,15 +1,19 @@
 package cursojava.thread;
 
+import java.awt.Dimension;
+
 import javax.swing.JDialog;
 
 public class TelaTimeThread extends JDialog{
 	
 	public TelaTimeThread() {/*Executa o que estiver dentro no momento da abertura instanciação desse objeto*/
 		// TODO Auto-generated constructor stub
-	
-	
+		setTitle("Minha tela de Thread com thread");
+		setSize(new Dimension(240,240));
+		setLocationRelativeTo(null);
+		setResizable(false);
 		
-
+		
 		setVisible(true);/*Torna a tela visivel para o usuario, sempre sera o ultimo comando*/
 	
 	}
