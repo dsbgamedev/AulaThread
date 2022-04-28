a361ef7080481203faa9edd68c25f7b0c3d09cce
Douglas <Douglas@Douglas>
2022-04-28 18:54:43 -0300

    Finalização

--- CursoJava/src/cursojava/thread/TelaTimeThread.java
+++ CursoJava/src/cursojava/thread/TelaTimeThread.java
@@ -5,6 +5,10 @@ import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
@@ -25,6 +29,25 @@ public class TelaTimeThread extends JDialog{
 	private JButton jButton = new JButton("Start");
 	private JButton jButton2 = new JButton("Stop");
 	
+	private Runnable thread1 = new Runnable() {
+		
+		@Override
+		public void run() {
+			while (true) {
+				mostraTempo.setText(new SimpleDateFormat("dd/MM/yyyy hh:mm.ss").
+						format(Calendar.getInstance().getTime()));
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+			
+		}
+	};
+	
+	private Thread thread1Time;
+	
 	
 	public TelaTimeThread() {/*Executa o que estiver dentro no momento da abertura instanciação desse objeto*/
 		// TODO Auto-generated constructor stub
@@ -32,7 +55,6 @@ public class TelaTimeThread extends JDialog{
 		setSize(new Dimension(240,240));
 		setLocationRelativeTo(null);
 		setResizable(false);
-		
 		
 		GridBagConstraints gridBagConstraints = new GridBagConstraints();/*Controlador de posicionamento de componentes*/
 		gridBagConstraints.gridx = 0; //Linha gridx
@@ -70,6 +92,35 @@ public class TelaTimeThread extends JDialog{
 		jButton2.setPreferredSize(new Dimension(92, 25));
 		gridBagConstraints.gridx ++;
 		jPanel.add(jButton2, gridBagConstraints);
+		
+		
+		jButton2.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+				thread1Time.stop();
+				
+				jButton.setEnabled(true);
+				jButton2.setEnabled(false);
+				
+			}
+		});
+		
+		jButton.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {/* Calcula o click no botao */
+				
+				thread1Time = new Thread(thread1);
+				thread1Time.start();
+				
+				jButton.setEnabled(false);
+				jButton2.setEnabled(true);
+				
+			}
+		});
+		
 		
 		add(jPanel, BorderLayout.WEST);
 		setVisible(true);/*Torna a tela visivel para o usuario, sempre sera o ultimo comando*/
