dcf9c08992ccf5253243e3e4c3413ee87fe33dd1
Douglas <Douglas@Douglas>
2022-04-28 19:03:31 -0300

    Classe Implem

--- CursoJava/src/cursojava/thread/ImplementacaoFilaThread.java
+++ CursoJava/src/cursojava/thread/ImplementacaoFilaThread.java
@@ -1,6 +1,20 @@
 package cursojava.thread;
 
+import java.util.concurrent.ConcurrentLinkedQueue;
+
 public class ImplementacaoFilaThread extends Thread {
 	
+	private static ConcurrentLinkedQueue<ObjetoFilaThread> pilha_fila = 
+			new ConcurrentLinkedQueue<ObjetoFilaThread>();
+	
+	public static void add(ObjetoFilaThread objetoFilaThread) {
+		pilha_fila.add(objetoFilaThread);
+	}
+	
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		super.run();
+	}
 
 }
