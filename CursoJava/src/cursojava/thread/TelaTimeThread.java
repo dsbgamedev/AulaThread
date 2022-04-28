7df270999bf0c30c0e91cfe19f65924dbca2b408
Douglas <Douglas@Douglas>
2022-04-28 17:34:03 -0300

    Criação painel

--- CursoJava/src/cursojava/thread/TelaTimeThread.java
+++ CursoJava/src/cursojava/thread/TelaTimeThread.java
@@ -1,10 +1,15 @@
 package cursojava.thread;
 
 import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 
 import javax.swing.JDialog;
+import javax.swing.JPanel;
 
 public class TelaTimeThread extends JDialog{
+	
+	private JPanel panel = new JPanel(new GridBagLayout());
 	
 	public TelaTimeThread() {/*Executa o que estiver dentro no momento da abertura instanciação desse objeto*/
 		// TODO Auto-generated constructor stub
@@ -12,6 +17,12 @@ public class TelaTimeThread extends JDialog{
 		setSize(new Dimension(240,240));
 		setLocationRelativeTo(null);
 		setResizable(false);
+		
+		
+		GridBagConstraints gridBagConstraints = new GridBagConstraints();
+		gridBagConstraints.gridx = 0; //Linha gridx
+		gridBagConstraints.gridy = 0; //Coluna gridy
+		
 		
 		
 		setVisible(true);/*Torna a tela visivel para o usuario, sempre sera o ultimo comando*/
