268318850ff198eb64e8cab22959be11433f4523
Douglas <Douglas@Douglas>
2022-04-28 18:18:17 -0300

    Finalização da tela

--- CursoJava/src/cursojava/thread/TelaTimeThread.java
+++ CursoJava/src/cursojava/thread/TelaTimeThread.java
@@ -4,6 +4,7 @@ import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.Insets;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
@@ -36,12 +37,17 @@ public class TelaTimeThread extends JDialog{
 		GridBagConstraints gridBagConstraints = new GridBagConstraints();/*Controlador de posicionamento de componentes*/
 		gridBagConstraints.gridx = 0; //Linha gridx
 		gridBagConstraints.gridy = 0; //Coluna gridy
+		gridBagConstraints.gridwidth = 2; //
+		gridBagConstraints.insets = new Insets(5, 10, 5, 5);
+		gridBagConstraints.anchor = GridBagConstraints.WEST; //Alinhar
+		
 		
 		descricaoHora.setPreferredSize(new Dimension(200,25));
 		jPanel.add(descricaoHora,gridBagConstraints);
 		
 		mostraTempo.setPreferredSize(new Dimension(200, 25));
 		gridBagConstraints.gridy ++; //Anda uma posição na coluna 
+		mostraTempo.setEditable(false);
 		jPanel.add(mostraTempo, gridBagConstraints);
 		
 		descricaoHora2.setPreferredSize(new Dimension(200, 25));
@@ -50,18 +56,20 @@ public class TelaTimeThread extends JDialog{
 
 		mostraTempo2.setPreferredSize(new Dimension(200, 25));
 		gridBagConstraints.gridy ++; //Anda uma posição na coluna 
+		mostraTempo2.setEditable(false);
 		jPanel.add(mostraTempo2, gridBagConstraints);
 		
+		
+		/************************************************************/
+		gridBagConstraints.gridwidth = 1;
 		
 		jButton.setPreferredSize(new Dimension(92, 25));
 		gridBagConstraints.gridy ++;
 		jPanel.add(jButton, gridBagConstraints);
 		
-		
 		jButton2.setPreferredSize(new Dimension(92, 25));
 		gridBagConstraints.gridx ++;
 		jPanel.add(jButton2, gridBagConstraints);
-		
 		
 		add(jPanel, BorderLayout.WEST);
 		setVisible(true);/*Torna a tela visivel para o usuario, sempre sera o ultimo comando*/
