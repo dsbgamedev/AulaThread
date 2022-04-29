deaeaeddde64470b69f8e678d8f9d06f5a43b171
Douglas <Douglas@Douglas>
2022-04-29 11:25:28 -0300

    Finalzado Aula 14.11

--- CursoJava/src/cursojava/thread/ImplementacaoFilaThread.java
+++ CursoJava/src/cursojava/thread/ImplementacaoFilaThread.java
@@ -1,6 +1,9 @@
 package cursojava.thread;
 
+import java.util.Iterator;
 import java.util.concurrent.ConcurrentLinkedQueue;
+
+//import javax.swing.text.html.HTMLDocument.Iterator;
 
 public class ImplementacaoFilaThread extends Thread {
 	
@@ -13,8 +16,36 @@ public class ImplementacaoFilaThread extends Thread {
 	
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
-		super.run();
+		Iterator iteracao = pilha_fila.iterator(); 
+		
+		synchronized (iteracao) {//Bloquear o acessar a esta lista por outro processo
+			
+			while (iteracao.hasNext()) {//enquanto conter dados a lista ira processar
+				
+				ObjetoFilaThread processar = (ObjetoFilaThread) iteracao.next();
+				
+				/* Processar uma lista rotina ou processo grande do java */
+				
+				/* Depois de processar limpar a lista */
+				
+				iteracao.remove();
+				
+				//Depois que remove colocar um tempo
+				try {
+					Thread.sleep(100);
+				} catch (InterruptedException e) {//Dar um tempo para descarga de memoria
+					e.printStackTrace();
+				}
+				
+			}
+			
+		}
+		
+		try {
+			Thread.sleep(1000);//Processou toda a lista da um tempo para limpeza de memoria
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
